d7297e3ddbe516930e9ef9f479e48bfa33cc05ea
Dinithi Amanda <amandadinithi930@gmail.com>
2025-09-22 06:21:31 +0530

    add comments

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,// Cloud name from environment variables
+  api_key: process.env.CLOUDINARY_API_KEY,// API key from environment variables
+  api_secret: process.env.CLOUDINARY_API_SECRET,// API secret from environment variables
 });
 
-module.exports = cloudinary;
+module.exports = cloudinary; // Export the configured Cloudinary instance
